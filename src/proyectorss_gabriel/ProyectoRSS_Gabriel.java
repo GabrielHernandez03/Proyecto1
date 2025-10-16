2adc5896846c5cf2e6e9696884b534da588f6147
GabrielHernandez03 <gabriel-hernandez@correo.unimet.edu.ve>
2025-10-15 23:58:43 -0400

    inicio

---
+++ src/proyectorss_gabriel/ProyectoRSS_Gabriel.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectorss_gabriel;
+
+/**
+ *
+ * @author gabriel
+ */
+public class ProyectoRSS_Gabriel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
