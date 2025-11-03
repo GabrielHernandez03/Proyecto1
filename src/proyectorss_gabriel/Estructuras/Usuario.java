f6add0fa0a66f58fc3ee249dd1017bf4efc06bc6
GabrielHernandez03 <gabriel-hernandez@correo.unimet.edu.ve>
2025-11-03 03:56:19 -0400

    Usuario

---
+++ src/proyectorss_gabriel/Estructuras/Usuario.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectorss_gabriel.Estructuras;
+
+/**
+ *
+ * @author gabriel
+ */
+public class Usuario {
+    String username;
+
+    public Usuario(String username) {
+        this.username = username;
+    }
+    
+    
+}
