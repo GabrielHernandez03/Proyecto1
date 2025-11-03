1a7bb3164c5e0b6b0e343084a5cdeb584b6c2ccf
GabrielHernandez03 <gabriel-hernandez@correo.unimet.edu.ve>
2025-11-03 03:55:04 -0400

    Vertice

---
+++ src/proyectorss_gabriel/Estructuras/Vertice.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectorss_gabriel.Estructuras;
+
+/**
+ *
+ * @author andre
+ */
+public class Vertice {
+    Nodo dato;
+    Vertice sig;
+
+    public Vertice(Nodo dato) {
+        this.dato = dato;
+        this.sig = null;
+    }
+    
+    
+}
