dd39dcba1bec37cac28d7c9c578e6343be4d97c8
GabrielHernandez03 <gabriel-hernandez@correo.unimet.edu.ve>
2025-11-03 03:57:31 -0400

    Grafo

---
+++ src/proyectorss_gabriel/Estructuras/Grafo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectorss_gabriel.Estructuras;
+
+/**
+ *
+ * @author andre
+ */
+public class Grafo {
+    Lista vertices;
+    int ususarios;
+
+    public Grafo() {
+        this.vertices = new Lista();
+        usuarios = 0;
+    }
+    
+    
+}
