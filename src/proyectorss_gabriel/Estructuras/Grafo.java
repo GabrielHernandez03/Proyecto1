c2084fb8a10e47e235adb96f85a7e934c7c34360
GabrielHernandez03 <gabriel-hernandez@correo.unimet.edu.ve>
2025-11-04 02:35:51 -0400

    Metodos Grafo

--- src/proyectorss_gabriel/Estructuras/Grafo.java
+++ src/proyectorss_gabriel/Estructuras/Grafo.java
@@ -4,18 +4,71 @@
  */
 package proyectorss_gabriel.Estructuras;
 
+import javax.swing.JOptionPane;
+
 /**
  *
- * @author andre
+ * @author gabriel
  */
 public class Grafo {
-    Lista vertices;
-    int ususarios;
+
+    Vertice primer_usuario;
+    Vertice ultimo_usuario;
+    int num_usuarios;
 
     public Grafo() {
-        this.vertices = new Lista();
-        usuarios = 0;
+        this.primer_usuario = null;
+        this.num_usuarios = 0;
+    }
+
+    public void nuevoUsuario(String usuario) {
+        Vertice existe = this.buscarUsuario(usuario);
+        if(existe == null){
+            Usuario nuevo_usuario = new Usuario(usuario);
+            if(primer_usuario == null){
+                this.primer_usuario = this.ultimo_usuario = new Vertice(nuevo_usuario);
+            }else{
+                this.ultimo_usuario.sig = new Vertice(nuevo_usuario);
+            }
+        }else{
+            JOptionPane.showMessageDialog(null, "Ya existe un usuario con ese nombre");
+        }
+    }
+
+    public Vertice buscarUsuario(String usuario) {
+        if (this.primer_usuario != null) {
+            Vertice pAux = this.primer_usuario;
+            while (pAux != null && !pAux.dato.equals(usuario)) {
+                pAux = pAux.sig;
+            }
+            return pAux;
+        } else {
+            return null;
+        }
     }
     
-    
+    public void eliminarUsuario(String usuario){
+       if (this.primer_usuario != null) {
+            Vertice pAux = this.primer_usuario;
+            if (pAux.dato.equals(usuario)) {
+                this.primer_usuario = this.primer_usuario.sig;
+            } else {
+                while (pAux != this.ultimo_usuario && !pAux.sig.dato.equals(usuario)) {
+                    pAux = pAux.sig;
+                }
+
+                if (pAux != this.ultimo_usuario) {
+                    pAux.sig = pAux.sig.sig;
+                }
+            }
+            
+            
+            pAux = this.primer_usuario;
+            while(pAux != null){
+                pAux.seguidos.eliminar(usuario);
+                pAux = pAux.sig;
+            }
+        }
+    }
+
 }
